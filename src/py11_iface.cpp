32069f344e4514855bac143f5722dd02c4d4e442
Imanol Etxezarreta <ietxezarretam@gmail.com>
2024-07-17 23:14:50 +0200

    Add scipy.sparse.csc_matrix input type support.

--- src/py11_iface.cpp
+++ src/py11_iface.cpp
@@ -10,7 +10,7 @@ namespace py = pybind11;
 // Bindings for the bpbp module
 PYBIND11_MODULE(BPOTF, BPOTF) {
    py::class_<OBPOTF>(BPOTF,"OBPOTF")
-      .def(py::init<py::array_t<uint8_t, py::array::f_style> const &, float const &>())
+      .def(py::init<py::object const &, float const &>())
       .def("print_object", &OBPOTF::print_object)
       .def("decode", &OBPOTF::decode);
 }
