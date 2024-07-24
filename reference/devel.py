d1e02700ca16f596195168ef74494918f94055cc
Imanol Etxezarreta <ietxezarretam@gmail.com>
2024-07-24 19:21:19 +0200

    Add tests for new decoding code types.

--- reference/devel.py
+++ reference/devel.py
@@ -1,5 +1,6 @@
-from module import bpbp
+import BPOTF
 import numpy as np
+from scipy import sparse
 
 def generate_random_matrix():
     N = np.random.randint(3, 10)  # Random number of rows between 1 and 10
@@ -11,19 +12,36 @@ def generate_random_matrix():
 
 # Example usage:
 mat = generate_random_matrix()
+scipy_mat = sparse.csc_matrix(mat)
 
 n, m = np.shape(mat)
 print("Matrix shape in python is: {}x{}".format(n, m))
 print(mat)
+n, m = scipy_mat.get_shape()
+print("Matrix shape in SCIPY python is: {}x{}".format(n, m))
+print(scipy_mat)
 
 #bpbp.koh_v2(mat)
 
-
-bpotf_obj = bpbp.OBPOTF(mat, 0.01)
+print(type(mat))
+bpotf_obj = BPOTF.OBPOTF(mat, 0.01)
+print("first created")
+print(type(scipy_mat))
+bpotf_obj_scipy = BPOTF.OBPOTF(scipy_mat, 0.01)
 
 bpotf_obj.print_object()
+bpotf_obj_scipy.print_object()
 
-fake_syndrome = np.random.randint(2, size=(n, 1), dtype=np.int32)
+fake_syndrome = np.random.randint(2, size=(n, 1), dtype=np.uint8)
 print(fake_syndrome)
 time = 0.0
-error = bpotf_obj.decode(fake_syndrome, time)
+error = bpotf_obj.decode(fake_syndrome)
+error_scipy = bpotf_obj_scipy.decode(fake_syndrome)
+
+assert np.allclose(error, error_scipy), "Errors not equal!"
+
+print("TEST OK")
+
+
+bpotf_obj_cln = BPOTF.OBPOTF(mat, 0.01, BPOTF.ECodeType.E_CLN)
+bpotf_obj_cln.decode(fake_syndrome)
